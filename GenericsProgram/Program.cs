b721313021ddc6b8b8ca5675e02bb044a8205200
SumitomShome <sumitomshome@gmail.com>
2020-10-28 01:01:20 +0530

    [Sumitom] Add: Method to find the maximum float number

--- GenericsProgram/Program.cs
+++ GenericsProgram/Program.cs
@@ -8,6 +8,8 @@ namespace GenericsProgram
             Console.WriteLine("Welcome to find the maximum");
             int output = MaximumCheck.MaximumIntegerNumber(11, 22, 33);
             Console.WriteLine(output);
+            double doubleoutput = MaximumCheck.MaximumFloatNumber(111.2, 22.45, 78.56);
+            Console.WriteLine(doubleoutput);
             Console.ReadKey();
         }
     }
