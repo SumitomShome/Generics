3646f44a74d8a2380ea7d236bf444d0c6c58bad0
SumitomShome <sumitomshome@gmail.com>
2020-10-28 00:51:24 +0530

    [Sumitom] Add: Method to find the maximum number

---
+++ GenericsProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace GenericsProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to find the maximum");
+            int output = MaximumCheck.MaximumIntegerNumber(11, 22, 33);
+            Console.WriteLine(output);
+            Console.ReadKey();
+        }
+    }
+}
