f3f02fa3d0369082071549226c2cdcf337cb3017
SumitomShome <sumitomshome@gmail.com>
2020-10-28 17:46:10 +0530

    [Sumitom] Add: Method to take more than three parameters

--- GenericsProgram/Program.cs
+++ GenericsProgram/Program.cs
@@ -1,17 +1,20 @@
 ﻿using System;
+using static GenericsProgram.FindMaximum;
 namespace GenericsProgram
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to find the maximum");
-            int output = MaximumCheck.MaximumIntegerNumber(11, 22, 33);
-            Console.WriteLine(output);
-            double doubleoutput = MaximumCheck.MaximumFloatNumber(111.2, 22.45, 78.56);
-            Console.WriteLine(doubleoutput);
-            string stringoutput = MaximumCheck.MaximumString("Apple", "Peach", "Banana");
-            Console.WriteLine(stringoutput);
+            int[] intArray = { 11, 22, 33, 12, 95, 84 };
+            GenericMaximum<int> genericInt = new GenericMaximum<int>(intArray);
+            genericInt.PrintMaxValue();
+            double[] doubleArray = { 111.1, 22.45, 78.56, 95.5, 84.8 };
+            GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
+            genericDouble.PrintMaxValue();
+            string[] stringArray = { "Apple", "Peach", "Banana", "Mango", "Jackfruit" };
+            GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
+            genericString.PrintMaxValue();
             Console.ReadKey();
         }
     }
